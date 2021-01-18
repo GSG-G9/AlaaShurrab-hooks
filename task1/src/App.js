02989483c0e384de5087848e20ec24d9f1b222fa
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-01-18 16:52:45 +0200

    task 3 this component is a todo list with a add button

--- task1/src/App.js
+++ task1/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Link, Route, Switch } from "react-router-dom";
 import Task1 from "./components/Task1";
 import Task2 from "./components/Task2";
-// import Task3 from "./components/Task3";
+import Task3 from "./components/Task3";
 // import Task4 from "./components/Task4";
 
 function App() {
@@ -12,6 +12,8 @@ function App() {
       <Link to="/task1">Task1</Link>
       <br></br>
       <Link to="/task2">Task2</Link>
+      <br></br>
+      <Link to="/task3">Task3</Link>
       <Switch>
         <Route path="/task1">
           <Task1 />
@@ -19,7 +21,9 @@ function App() {
         <Route path="/task2">
           <Task2 />
         </Route>
-        {/* <Route path="/task3" component={Task3} /> */}
+        <Route path="/task3">
+          <Task3 />
+        </Route>
         {/* <Route path="/task4" component={Task4} /> */}
       </Switch>
     </div>
